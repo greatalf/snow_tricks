9b0119a28e14468ddaa4a87ea6c1f125d9218e0c
Greatalf <avril.laurent974@yahoo.fr>
2018-08-28 09:34:28 +0400

    home and figures display blank pages

---
+++ src/Controller/TricksController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\FigureRepository;
+use App\Entity\Figure;
+use App\Entity\Category;
+use App\Form\FigureType;
+
+class TricksController extends AbstractController
+{ 
+    /**
+     *@Route("/", name="home")
+     */
+    public function home(FigureRepository $repo)
+    {        
+        $figures = $repo->findAll();
+
+        return $this->render('tricks/home.html.twig', ['figures' => $figures]);
+    }   
+}
+
