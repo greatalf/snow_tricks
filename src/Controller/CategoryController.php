cdb79e1719fa43ae0c4a05cb62af31e439be4ab8
Greatalf <avril.laurent974@yahoo.fr>
2018-12-06 18:03:20 +0100

    create category feature

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Form\CategoryType;
+use App\Entity\Category;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/categories", name="category")
+     */
+    public function index(Request $request, Objectmanager $manager)
+    {
+        $category = new Category;
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {            
+
+            $manager->persist($category);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'La catégorie ' .  $category->getName() . ' a bien été créée'
+            );
+            return $this->redirectToRoute('tricks_create');
+        }
+
+        return $this->render('category/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Migrations/Version20181206104246.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181206104246 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE comment CHANGE figure_id figure_id INT NOT NULL');
+        $this->addSql('ALTER TABLE visual RENAME INDEX idx_c53d045f4f34d596 TO IDX_EBC9881F5C011B5');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE comment CHANGE figure_id figure_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE visual RENAME INDEX idx_ebc9881f5c011b5 TO IDX_C53D045F4F34D596');
+    }
+}
