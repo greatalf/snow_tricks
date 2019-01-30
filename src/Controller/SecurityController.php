6e96f12fe3602a173de52b34aaaef2ec74c4727b
laurent <avril.laurent974@yahoo.fr>
2019-01-30 17:52:18 +0100

    an other test credential

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -186,8 +186,8 @@ class SecurityController extends AbstractController
             }
 
             //Créer le transport
-            $transport = (new \Swift_SmtpTransport('sandbox0171cc4c4fea44a09285cb3b9ce251d4.mailgun.org', 587)) 
-                        ->setUsername('postmaster@sandbox0171cc4c4fea44a09285cb3b9ce251d4.mailgun.org')
+            $transport = (new \Swift_SmtpTransport('smtp.mailgun.org', 587)) 
+                        ->setUsername('sandbox0171cc4c4fea44a09285cb3b9ce251d4.mailgun.org')
                         ->setPassword('azertyuiop%')
             ; 
 
